bfbbacec9781edebbf961747d9ecfdfa31235866
AlberthGarcia <alberthperezgarcia@gmail.com>
2022-04-21 11:42:11 -0500

    funciones para verificar tablas y crear
    Funciones para crear tablas y verificar su existencia

--- db/conecction.go
+++ db/conecction.go
@@ -2,15 +2,17 @@ package db
 
 import (
 	"database/sql"
+	"fmt"
 	"log"
 
 	_ "github.com/go-sql-driver/mysql"
 )
 
-var connection = "root:@tcp(localhost:3306)/bdGym"
+var connection = "root:@tcp(localhost:3306)/dbGym"
 
 var db *sql.DB
 
+//func to open the connection
 func Open() {
 	connection, err := sql.Open("mysql", connection)
 	if err != nil {
@@ -19,17 +21,45 @@ func Open() {
 	db = connection
 }
 
+//func to verify the connection
 func Ping() {
 	if err := db.Ping(); err != nil {
 		log.Println("Connection still open")
 	}
 }
 
+//func to close the connection
 func Close() {
 	err := db.Close()
 	if err != nil {
 		log.Fatal("Cannot close the db", err)
 	}
+}
+
+//func to create a table
+func CreateTable(schema string, tableName string) {
+	//If do not exists the table, We're going to create
+	if !ExistsTable(tableName) {
+		_, err := Exec(schema)
+		if err != nil {
+			log.Fatalln(err, "Cannot create the table")
+		}
+	} else {
+		log.Println("Table already exists")
+	}
+
+}
+
+//func to verify if exists a table in the DB
+func ExistsTable(tableName string) bool {
+	sql := fmt.Sprintf("Show tables like '%s'", tableName)
+	rs, err := Query(sql)
+	if err != nil {
+		log.Fatalln("Error: ", err)
+	}
+
+	//if exists, return true
+	return rs.Next()
 }
 
 func Exec(query string, args ...any) (sql.Result, error) {
--- main.go
+++ main.go
@@ -1,7 +1,5 @@
 package main
 
-import "packs/db"
-
 func main() {
 	//cargar archivos estaticos
 	// http.Handle("/static", http.StripPrefix("/static", nil))
@@ -9,6 +7,5 @@ func main() {
 	// //Rutas para el login
 	// http.HandleFunc("/login", handlers.Login)
 	// http.ListenAndServe(":8080", nil)
-	db.Open()
-	db.Close()
+
 }
